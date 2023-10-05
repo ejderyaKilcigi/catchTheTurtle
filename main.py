906c5678948e3d67a305051a723006bfd0704991
Gokhan Serce <mxpane@gmail.com>
2023-10-05 11:51:31 +0300

    Score table added

--- main.py
+++ main.py
@@ -6,18 +6,37 @@ turtleScreen.bgcolor("#3A8CD5")
 turtleScreen.title("Catch The Turtle")
 
 lion = turtle.Turtle()
+panter = turtle.Turtle()
+
 lion.penup()
 lion.shape("turtle")
 lion.speed(10)
 
+panter.penup()
+panter.hideturtle()
+panter .goto(0,250)
+style = ('Courier', 30, 'bold')
+plus = 0
 
 
+
+def Score():
+
+    global plus
+    plus+= 1
+    panter.clear()
+    panter.write(f"SKOR : {plus}", font=style, align="center")
+
+
+lion.onclick(lambda x, y: Score())
 
 for i in range(10):
     x = random.randint(-100, 100)
     y = random.randint(-100, 100)
     lion.goto(x, y)
     turtleScreen.delay(1000)
+    turtleScreen.update()
+
 
 
 
